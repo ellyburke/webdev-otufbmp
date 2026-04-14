478262886296dfb9c996ae073d3cc44b8ac778f3
Ellycea Burke <ellyburkee12@bell.net>
2026-04-13 20:35:20 -0400

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
 import CommentInput from './components/CommentInput.vue';
 import CommentSection from './components/CommentSection.vue';
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
 </script>
 
 <template>
-  <main>
-    <CommentSection />
-  </main>
+  <Navbar />
+  
+  <!-- To wrap the sidebar and the main content so they are side by side -->
+  <div class="content-wrapper">
+    <Sidebar />
+    <main>
+      <!-- <CommentSection /> -->
+      
+    </main>
+  </div>
 </template>
 
 <style scoped>
+.content-wrapper{
+  display: flex;
+}
 </style>
