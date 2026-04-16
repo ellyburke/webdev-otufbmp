cf2fafb7ec64a78e547d66181c6695aa5e9178d3
Ellycea Burke <ellyburkee12@bell.net>
2026-04-16 19:20:53 -0400

    merging to main

--- src/services/PostsService.ts
+++ src/services/PostsService.ts
@@ -4,24 +4,8 @@ export class PostsService {
   // Get all the posts from the server
   async loadPostsFromServer() {
     try {
-      //   const response = await fetch('http://localhost:3000/all-items')
-      // const data = await response.json();
-      const data = [
-        {
-          id: 1,
-          name: 'Item 1',
-          price: 10.99,
-          description: 'Description for item 1',
-          image_url: 'https://via.placeholder.com/150',
-        },
-        {
-          id: 2,
-          name: 'Item 2',
-          price: 20.99,
-          description: 'Description for item 2',
-          image_url: 'https://via.placeholder.com/150',
-        },
-      ]
+      const response = await fetch('http://localhost:3000/all-items')
+      const data = await response.json()
       return data
     } catch (error) {
       console.error('Error loading posts:', error)
@@ -41,25 +25,8 @@ export class PostsService {
 
   async getFavouritePosts(userId: number) {
     // This function will fetch all favourited posts for a specific user from the server
-    // return fetch(`http://localhost:3000/favourites?userId=${userId}`)
-    //   .then((response) => response.json())
-    //   .catch((error) => console.error('Error fetching favourited posts:', error))
-
-    return [
-      {
-        id: 4,
-        name: 'Item 4',
-        price: 40.99,
-        description: 'Description for item 4',
-        image_url: 'https://via.placeholder.com/150',
-      },
-      {
-        id: 3,
-        name: 'Item 3',
-        price: 30.99,
-        description: 'Description for item 3',
-        image_url: 'https://via.placeholder.com/150',
-      },
-    ]
+    return fetch(`http://localhost:3000/favourites?userId=${userId}`)
+      .then((response) => response.json())
+      .catch((error) => console.error('Error fetching favourited posts:', error))
   }
 }
