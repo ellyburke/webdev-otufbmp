91bd556926a2103e555f73a325dd0893ded177d8
JeremyT21 <thummeljeremy@gmail.com>
2026-04-17 13:43:02 -0400

    Map explanation view.

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -5,7 +5,23 @@ import Map from '../components/Map.vue'
 </script>
 
 <template>
+  <br>
+  <h1 class="title">Welcome to a World of Possibilities!</h1>
   <div>
     <Map></Map>
   </div>
+  <br>
+  <p>
+    Here you can see the most active countries using <span class="has-text-weight-bold">quick sale</span>.
+    <br><br>
+    The darker the shade of blue the location on the map is, the more items have been listed for sale there!
+    <br><br>
+    List whatever items you want to sell, and maybe your country will become a more darker blue.
+  </p>
 </template>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
